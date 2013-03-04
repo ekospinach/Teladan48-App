738d817ee245108cdad61f590f791322620b23da
Ridwan Aditama <reidzeibel@gmail.com>
2013-03-04 19:57:05 +0700

    -Add .gitignore to /bin and /gen -Cleanup

--- gen/com/ridwanadit/teladan48/R.java
+++ gen/com/ridwanadit/teladan48/R.java
@@ -11,13 +11,14 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int ic_tab_photos=0x7f020001;
-        public static final int ic_tab_photos_selected=0x7f020002;
-        public static final int ic_tab_photos_unselected=0x7f020003;
-        public static final int ic_tab_videos=0x7f020004;
-        public static final int ic_tab_videos_selected=0x7f020005;
-        public static final int ic_tab_videos_unselected=0x7f020006;
+        public static final int gravatar_default=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int ic_tab_photos=0x7f020002;
+        public static final int ic_tab_photos_selected=0x7f020003;
+        public static final int ic_tab_photos_unselected=0x7f020004;
+        public static final int ic_tab_videos=0x7f020005;
+        public static final int ic_tab_videos_selected=0x7f020006;
+        public static final int ic_tab_videos_unselected=0x7f020007;
     }
     public static final class layout {
         public static final int main=0x7f030000;
